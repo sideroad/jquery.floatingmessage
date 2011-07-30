c0e23f92fd03389657afe2acc30d870323bdfb50
sideroad <sideroad.jp@gmail.com>
2011-07-30 10:39:00 +0900

    change position to fixed

--- src/jquery.floatingmessage.js
+++ src/jquery.floatingmessage.js
@@ -12,90 +12,80 @@
  * 
  */
 ( function( $ ) {
-    var range = {
-            top : {
-                left : 10,
-                right : 10
-            },
-            bottom : {
-                left : 10,
-                right : 10
-            }
+    var ranges = {
+            "top-left" : 10,
+            "top-right" : 10,
+            "bottom-left" : 10,
+            "bottom-right" : 10
         },
-        right = 10,
-        fContainer = [],
-        scrollTop = 0,
-        remove;
-
-    $( document ).ready( function() {
-        scrollTop = $( window ).scrollTop();
-        $( window ).scroll( function() {
-            scrollTop = $( window ).scrollTop();
-            for ( i = 0; i < fContainer.length; i++ ) {
-                var animate = {};
-                var e = fContainer[i];
-                animate[e.verticalAlign] = e.range;
-                if ( e.verticalAlign == "top" )
-                    animate[e.verticalAlign] += scrollTop;
-                else animate[e.verticalAlign] -= scrollTop;
-
-                e.elem.animate( animate, {
-                    duration : e.moveEaseTime,
-                    easing : e.moveEasing,
-                    queue : false
-                } );
-            }
-        } );
-    } );
-
-    $.floatingMessage = function( message, options ) {
-        var id = "jqueryFloatingMessage" + new Date().getTime(),
-            elem = $( '<div id="'+id+'" class="ui-widget-content ui-corner-all floating-message"></div>' ),
-            css = {},
-            timerId = false;
-        
-        remove = function( elem, options ) {
-            if ( timerId ) clearTimeout( timerId );
-            var e,
+        opt = {},
+        align = 10,
+        container = {
+		    "top-left" : [],
+		    "top-right" : [],
+		    "bottom-left" : [],
+		    "bottom-right" : []
+        },
+        remove = function( elem ) {
+            var  i,
                 id = elem.attr("id"),
+                options = opt[id],
                 animate = {},
                 deleteIndex = 0,
-                orange = ( options.height + options.margin + ( options.padding * 2 ) );
+                range = options.range,
+                position = options.position,
+                stuffEaseTime = options.stuffEaseTime,
+                stuffEasing = options.stuffEasing,
+                hide = options.hide,
+                close = options.close,
+                distance = options.height + options.margin + ( options.padding * 2 ) ,
+                list = container[options.position];
+
+            if ( options.timerId ) clearTimeout( options.timerId );
                 
-            for ( i = 0; i < fContainer.length; i++ ) {
-                e = fContainer[i];
-                if ( id === e.id ) deleteIndex = i;
-                if ( e.range > options.range && e.align == options.align
-                        && e.verticalAlign == options.verticalAlign ) {
-                    e.range -= orange;
-                    if ( e.range < 0 ) e.range = 0;
-                    animate[e.verticalAlign] = e.range;
-                    if ( e.verticalAlign == "top" )
-                        animate[e.verticalAlign] += scrollTop;
-                    else animate[e.verticalAlign] -= scrollTop;
-                    e.elem.animate( animate, {
-                        duration : options.stuffEaseTime,
-                        easing : options.stuffEasing,
+            for ( i = 0; i < list.length; i++ ) {
+                options = list[i];
+                if ( id === options.id ) {
+                	    deleteIndex = i;
+                	    continue;
+                }
+                if ( options.position == position && options.range > range ) {
+                	options.range -= distance;
+                    if ( options.range < 0 ) {
+                    	options.range = 0;
+                    }
+                    animate[options.verticalAlign] = options.range;
+                    options.elem.animate( animate, {
+                        duration : stuffEaseTime,
+                        easing : stuffEasing,
                         queue : false
                     } );
                 }
             }
-            fContainer.splice( deleteIndex, 1 );
-            range[options.verticalAlign][options.align] -= ( options.height + options.margin + ( options.padding * 2 ) );
-            if ( range[options.verticalAlign][options.align] < 0 )
-                range[options.verticalAlign][options.align] = 10;
-            elem.hide( options.hide, function() {
+            list.splice( deleteIndex, 1 );
+            delete opt[id];
+            ranges[position] -= distance;
+            if ( ranges[position] < 0 ) {
+            	    ranges[position] = 10;
+            }
+            elem.hide( hide, function() {
                 $( this ).remove();
-                if ( options.close ) options.close();
+                if ( close ) close();
             } );
         };
-            
-        options = options || {};
+
+    $.floatingMessage = function( message, options ) {
+        var id = "jqueryFloatingMessage" + new Date().getTime() + Math.random(),
+            elem = $( '<div id="'+id+'" class="ui-widget-content ui-corner-all ui-floating-message"></div>' ),
+            css = {};
+        
 
         // default setting
+        options = options || {};
         options = $.extend( true, {
-            align : (options.position || "left-top").split("-")[0],
-            verticalAlign : (options.position || "left-top").split("-")[1],
+        	    position : "top-left",
+            verticalAlign : (options.position || "top-left").split("-")[0],
+            align : (options.position || "top-left").split("-")[1],
             width : 300,
             height : 50,
             time : false,
@@ -103,17 +93,17 @@
             hide : "drop",
             padding : 10,
             margin : 10,
-            stuffEaseTime : 1000,
+            stuffEaseTime : 500,
             stuffEasing : "easeOutBounce",
-            moveEaseTime : 500,
-            moveEasing : "easeInOutCubic",
             body : $( "<div></div>" ),
             close : false,
             click : remove,
             elem : elem,
+            timerId : false,
             id : id
         }, options );
-        options.range = range[options.verticalAlign][options.align];
+        opt[id] = options;
+        options.range = ranges[options.position];
 
         if ( message ) options.body.html( message.replace( /\n/g, "<br />" ) );
         if ( options.className ) elem.addClass(options.className);
@@ -121,40 +111,44 @@
         css = {
             width : options.width + "px",
             height : options.height + "px",
-            position : "absolute",
+            position : "fixed",
             padding : options.padding + "px"
         };
-        css[options.verticalAlign] = range[options.verticalAlign][options.align];
-        css[options.align] = right;
-        if ( options.verticalAlign == "top" ) {
-            css[options.verticalAlign] += scrollTop;
-        } else {
-            css[options.verticalAlign] -= scrollTop;
-        }
+        css[options.verticalAlign] = ranges[options.position];
+        css[options.align] = align;
+        
         elem.css( css ).append( options.body );
 
-        if ( options.time ) timerId = setTimeout( function(){options.click( elem, options )}, options.time );
-        if ( options.click ) elem.bind( "click.fms", function(){options.click( elem, options )} );
-        elem.bind( "destroy.fms", function(){ remove( elem, options )} );
+        if ( options.time ) {
+        	    options.timerId = setTimeout( function(){
+        	    		options.click( elem );
+        	    	}, options.time );
+        }
+        if ( options.click ) {
+        		elem.bind( "click.fms", function(){
+        			options.click( elem );
+        		} );
+        }
+        elem.bind( "destroy.fms", function(){ remove( elem );} );
 
         $( document.body ).append( elem );
         elem.show( options.show );
-        fContainer.push( options );
-        range[options.verticalAlign][options.align] += ( options.height + options.margin + ( options.padding * 2 ) );
+        container[options.position].push( options );
+        ranges[options.position] += ( options.height + options.margin + ( options.padding * 2 ) );
         
         return options.body;
 
-    }
+    };
 
     $.fn.floatingMessage = function( options ) {
         return this.each( function() {
             if ( typeof options == "string" ) {
-                $( this ).parent(".floating-message").trigger( options+".fms" );
+                $( this ).parent(".ui-floating-message").trigger( options+".fms" );
             } else {
                 options = options || {};
                 options.body = this;
                 $.floatingMessage( false, options );
             }
         } );
-    }
+    };
 } )( jQuery );
